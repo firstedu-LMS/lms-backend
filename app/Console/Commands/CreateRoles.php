61cc97078266b60807a720837b8f3540c15ea7fe
pokemon333 <myatkyawt974@gmail.com>
2023-07-23 16:08:00 +0630

    spatie added and make a command

---
+++ app/Console/Commands/CreateRoles.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Permission\Models\Role;
+
+class CreateRoles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-roles';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'client']);
+        Role::create(['name' => 'customer']);
+    }
+}
