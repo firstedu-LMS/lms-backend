6e09ae8261f54027026bb982c7ac14d7b0d87f89
Axel Philip <axel8102005@gmail.com>
2023-07-25 18:37:44 +0630

    cvform

---
+++ app/Http/Resources/CvFormResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CvFormResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => $this->user,
+            'file' => $this->file,
+            'created_at' => $this->created_at->format('d-m-y'),
+            'updated_at' => $this->updated_at->format('d-m-y'),
+        ];
+    }
+}
---
+++ app/Models/CvForm.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CvForm extends Model
+{
+    protected $guarded = [];
+    use HasFactory;
+    public function file()
+    {
+        return $this->belongsTo(File::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
