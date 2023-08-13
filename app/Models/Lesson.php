e907d1d3c502ff4799cd6240ae3ed8da94a850d9
pokemon333 <myatkyawt974@gmail.com>
2023-08-13 18:17:29 +0630

    relationship

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -9,4 +9,23 @@ class Lesson extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    /**
+     * Get the user that owns the Lesson
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function week()
+    {
+        return $this->belongsTo(Week::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function batch()
+    {
+        return $this->belongsTo(Batch::class);
+    }
 }
