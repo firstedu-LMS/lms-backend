81de0ef18b5643bc82f5517e06e7fb8a1821831b
pokemon333 <myatkyawt974@gmail.com>
2023-08-14 22:32:40 +0630

    make a database backend system

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Course;
 use App\Models\Instructor;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -18,26 +19,64 @@ use Illuminate\Support\Facades\Route;
 //This route will generate 5 
 Route::get('/', function () {
     $numberOfInstructors = 5;
-    for ($i=0; $i < $numberOfInstructors; $i++) { 
+    for ($i = 0; $i < $numberOfInstructors; $i++) {
         $instructor = Instructor::select('instructor_id')
-        ->orderByDesc('instructor_id')
-        ->value('instructor_id');
-        $instructorIdOnly = substr($instructor,2);
-        if($instructorIdOnly){
-            "I-". $instructorId = str_pad((int)$instructorIdOnly +1 ,4,"0",STR_PAD_LEFT);
-        }else{
-            "I-".$instructorId = config('instructorid.id');
+            ->orderByDesc('instructor_id')
+            ->value('instructor_id');
+        $instructorIdOnly = substr($instructor, 2);
+        if ($instructorIdOnly) {
+            "I-" . $instructorId = str_pad((int)$instructorIdOnly + 1, 4, "0", STR_PAD_LEFT);
+        } else {
+            "I-" . $instructorId = config('instructorid.id');
         }
-            $user = new User();
-            $user->name = fake()->name();
-            $user->email = fake()->unique()->safeEmail();
-            $user->password = Hash::make('internet');
-            $user->save();
-            $instructor = new Instructor();
-            $instructor->user_id = $user->id;
-            $instructor->instructor_id = $instructorId;
-            $instructor->cv_id = 1;
-            $instructor->save();
+        $user = new User();
+        $user->name = fake()->name();
+        $user->email = fake()->unique()->safeEmail();
+        $user->password = Hash::make('internet');
+        $user->save();
+        $instructor = new Instructor();
+        $instructor->user_id = $user->id;
+        $instructor->instructor_id = $instructorId;
+        $instructor->cv_id = 1;
+        $instructor->save();
     }
     return "Finish";
+});
+
+
+Route::get('/sql', function () {
+    $instructors =  Instructor::all();
+    echo  $instructors;
+    $i = 0;
+    $arryLength = count($instructors);
+    foreach ($instructors as $index=>$instructor) {
+        $i = $index;
+        $data = json_decode($instructor, true);
+        $tableName = "instructors";
+        $columns = "`" . implode("`, `", array_keys($data)) . "`";
+        $values = []; 
+        foreach ($data as $value) { 
+            if ($value === null) {
+                $values[] = "NULL";
+            } else {
+                $values[] = "'" . $value . "'";
+            }
+        }
+        $valuesString = implode(", ", $values);
+        $filePath =  base_path('app') . DIRECTORY_SEPARATOR ."backup". DIRECTORY_SEPARATOR."backup.sql";
+        $dir =  base_path('app') . DIRECTORY_SEPARATOR ."backup";
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777,true);
+        }
+        if (!file_exists($filePath) ) {
+            $header  = "INSERT INTO `$tableName` ($columns) VALUES\n";
+             file_put_contents($filePath, $header ,FILE_APPEND);
+        }
+        if ($i == $arryLength - 1) {
+            $sql = "  ($valuesString);"."\n";
+        }else{
+            $sql = "  ($valuesString),"."\n";
+        }
+        file_put_contents($filePath, $sql ,FILE_APPEND);
+    }
 });
