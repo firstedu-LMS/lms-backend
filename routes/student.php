b7bfc3c44c0db73749f683f63a5bcd4b51759519
Virnus <108821270+pokemon333@users.noreply.github.com>
2023-09-09 23:10:45 +0630

    Update student.php

--- routes/student.php
+++ routes/student.php
@@ -5,7 +5,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/profile', [StudentController::class, 'profile']);
-});
-    Route::patch('/profile/{student}', [StudentController::class, 'update']);
+// Route::middleware('auth:sanctum')->group(function () {
+   
+// });
+Route::get('/profile', [StudentController::class, 'profile']);
+Route::patch('/profile/{student}', [StudentController::class, 'update']);
