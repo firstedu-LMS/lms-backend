ee6d05635384d519a9e04e66d325ddcdbf52aae5
Vadsha <108509880+Vadsha@users.noreply.github.com>
2023-09-13 15:26:44 +0630

    Update student.php

--- routes/student.php
+++ routes/student.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->group(function () {
 
 // });
-Route::get('/profile', [StudentController::class, 'profile']);
+Route::get('/user', [StudentController::class, 'profile']);
 Route::patch('/profile/{student}', [StudentController::class, 'update']);
 Route::post('/enrollment',[StudentController::class, 'enrollment']);
