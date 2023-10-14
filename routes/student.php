431e7b23fd91872f5f289dec4cf525f674a672b0
Virnus <108821270+pokemon333@users.noreply.github.com>
2023-10-14 22:34:08 +0630

    Update student.php

--- routes/student.php
+++ routes/student.php
@@ -10,6 +10,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::patch('/user/{student}', [StudentController::class, 'update']);
     Route::get('/course-per-students/{student_id}', [StudentController::class, 'course_per_students']);
     Route::post('/lesson-completions', [StudentController::class, 'lessonCompletion']);
-    Route::get('/get-lessons-of-week', [StudentController::class, 'studentGetlessonsOfWeek']);
-    Route::get('/get-weeks-of-course', [StudentController::class, 'studentGetweeksOfCourse']);
+    Route::get('/get-lessons-of-week/{student_id}/{course_id}/{batch_id}/{week_id}', [StudentController::class, 'studentGetlessonsOfWeek']);
+    Route::get('/get-weeks-of-course/{student_id}/{course_id}/{batch_id}', [StudentController::class, 'studentGetweeksOfCourse']);
 });
