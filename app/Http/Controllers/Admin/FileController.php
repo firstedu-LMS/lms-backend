d01d605dcc3ebf76f5ed5ea1fabed65a7cdf23d2
Virnus <108821270+pokemon333@users.noreply.github.com>
2023-10-15 17:16:38 +0630

    fix form request

--- app/Http/Controllers/Admin/FileController.php
+++ app/Http/Controllers/Admin/FileController.php
@@ -5,20 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\Models\File;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
+use App\Http\Requests\FileRequest;
 use Illuminate\Support\Facades\Validator;
 
 use function App\Helper\storeFile;
 
 class FileController extends BaseController
 {
-    public function store(Request $request){
-        $validator = Validator::make($request->all(),[
-            $request->file('file') ? 'file' : 'assignment' => 'required|mimes:pdf',
-        ]
-        );
-        if($validator->fails()) {
-            return $this->error($validator->errors(),"Validation Error",config('http_status_code.unprocessable_content'));
-        }
+    public function store(FileRequest $request){
         if($request->file('assignment')) {
             return $this->handleFileCreate($request,'assignment');
         }else {
--- app/Http/Requests/ImageRequest.php
+++ app/Http/Requests/ImageRequest.php
@@ -2,36 +2,40 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rules\File;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Validation\Rules\File;
 use Illuminate\Validation\ValidationException;
 
-class VideoRequest extends FormRequest
+class ImageRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
+
+   
     public function authorize(): bool
     {
         return true;
     }
 
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
     public function rules(): array
     {
         return [
-            'video'  => [
-                            'required',
-                            File::types(['mp4',])
-                        ],
+            $this->file('course_image') ? 'course_image' : 'user_image' => ['required',File::types(['png','jpeg','jpg'])],
         ];
     }
-    
+
     protected function failedValidation(Validator $validator)
     {
         throw new ValidationException($validator,response()->json([
             "errors" => $validator->errors(),
-            "messages" => "Validation Errors",
+            "message" => "Validation Error"
         ],config('http_status_code.unprocessable_content')));
     }
 }
