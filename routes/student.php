445a91e7f1891b9211e478b4d5890bf66ce8bc63
pokemon333 <myatkyawt974@gmail.com>
2023-10-25 22:16:41 +0630

    bug:student can not enroll course

--- routes/student.php
+++ routes/student.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\Student\StudentController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [StudentController::class, 'profile']);
-    Route::patch('/user/{student}', [StudentController::class, 'update']);
     Route::get('/course-per-students/{student_id}', [StudentController::class, 'course_per_students']);
-    Route::post('/lesson-completions', [StudentController::class, 'lessonCompletion']);
     Route::get('/get-lessons-of-week/{student_id}/{course_id}/{batch_id}/{week_id}', [StudentController::class, 'studentGetlessonsOfWeek']);
     Route::get('/get-weeks-of-course/{student_id}/{course_id}/{batch_id}', [StudentController::class, 'studentGetweeksOfCourse']);
+    Route::post('/lesson-completions', [StudentController::class, 'lessonCompletion']);
+    Route::patch('/user/{student}', [StudentController::class, 'update']);
 });
