37f8389dc02260707cc7c8ba4ada714b84bf60cf
pokemon333 <myatkyawt974@gmail.com>
2023-10-28 23:22:01 +0630

    get questions of lesson

---
+++ app/Http/Controllers/Student/QuestionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Controllers\Controller;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends BaseController
+{
+   public function getLessonQuestions($lesson_id){
+        $question = Question::where('lesson_id',$lesson_id)->get();
+        if ($question->count() == 0 ) {
+            return $this->error([], 'there is no questions', config('http_status_code.not_found'));
+        }
+        return $this->success($question,'questions of lesson',config('http_status_code.ok'));
+   }
+//    public function submission() {
+//         return $this->success($question,'questions of lesson',config('http_status_code.ok'));
+//    }
+
+}
