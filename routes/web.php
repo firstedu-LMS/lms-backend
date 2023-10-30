b60e3cbd559209841e86aa0522e6a83fe760e8f2
pokemon333 <myatkyawt974@gmail.com>
2023-10-30 14:41:47 +0630

    submission

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,5 @@
 <?php
 
-use App\Models\User;
-use App\Models\Week;
-use App\Models\Batch;
-use App\Models\Career;
-use App\Models\Course;
-use App\Models\CvForm;
-use App\Models\Student;
-use App\Models\Assignment;
-use App\Models\Enrollment;
-use App\Models\Instructor;
-use App\Models\Application;
-use function App\Helper\hello;
-use App\Models\CourseCompletion;
-use App\Models\CoursePerStudent;
-use function App\Helper\storeFile;
-use App\Mail\AdminLoginVertifyMail;
-use App\Utils\CheckToDeleteService;
-
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
