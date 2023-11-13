c44765b29340e9d41b571db2a96ceae87a82be07
Axel Philip <axel8102005@gmail.com>
2023-11-13 12:24:34 +0630

    c++

--- database/seeders/BatchSeeder.php
+++ database/seeders/BatchSeeder.php
@@ -14,106 +14,21 @@ class BatchSeeder extends Seeder
      */
     public function run(): void
     {
-        $number = 1;
         for($i =1 ; $i <=10 ; $i++) {
-            DB::table('batches')->insert([
-                'name' => 'Batch-' .$number++,
-                "course_id" => 1,
-                "instructor_id" => 1,
-                "start_date" => Carbon::now()->toDateString(),
-                "end_date" => Carbon::now()->addDay(30)->toDateString(),
-                "start_time" => Carbon::now()->toTimeString(),
-                "end_time" => Carbon::now()->addHour(3)->toTimeString(),
-                "status" => fake()->numberBetween(0,1),
-                "created_at" => Carbon::now(),
-                "updated_at" => Carbon::now()
-            ]);
-            DB::table('batches')->insert([
-                'name' => 'Batch-' .$number-1,
-                "course_id" => 2,
-                "instructor_id" => 2,
-                "start_date" => Carbon::now()->toDateString(),
-                "end_date" => Carbon::now()->addDay(30)->toDateString(),
-                "start_time" => Carbon::now()->toTimeString(),
-                "end_time" => Carbon::now()->addHour(3)->toTimeString(),
-                "status" => fake()->numberBetween(0,1),
-                "created_at" => Carbon::now(),
-                "updated_at" => Carbon::now()
-            ]);
+            for($c = 1; $c <=2 ; $c++) {
+                DB::table('batches')->insert([
+                    'name' => 'Batch-' .$i++,
+                    "course_id" => $c,
+                    "instructor_id" => $c,
+                    "start_date" => Carbon::now()->toDateString(),
+                    "end_date" => Carbon::now()->addDay(30)->toDateString(),
+                    "start_time" => Carbon::now()->toTimeString(),
+                    "end_time" => Carbon::now()->addHour(3)->toTimeString(),
+                    "status" => fake()->numberBetween(0,1),
+                    "created_at" => Carbon::now(),
+                    "updated_at" => Carbon::now()
+                ]);
+            }
         }
-        // DB::table('batches')->insert([
-        //     [
-        //         'name' => 'Batch-' .$number++,
-        //         "course_id" => "1",
-        //         "instructor_id" => "1",
-        //         "start_date" => Carbon::now()->toDateString(),
-        //         "end_date" => Carbon::now()->addDay(30)->toDateString(),
-        //         "start_time" => Carbon::now()->toTimeString(),
-        //         "end_time" => Carbon::now()->addHour(3)->toTimeString(),
-        //         "status" => "1",
-        //         "created_at" => Carbon::now(),
-        //         "updated_at" => Carbon::now()
-        //     ],
-            // [
-            //     "name" => "Batch-1",
-            //     "course_id" => "2",
-            //     "instructor_id" => "2",
-            //     "start_date" => Carbon::now()->toDateString(),
-            //     "end_date" => Carbon::now()->addDay(30)->toDateString(),
-            //     "start_time" => Carbon::now()->toTimeString(),
-            //     "end_time" => Carbon::now()->addHours(3)->toTimeString(),
-            //     "status" => "1",
-            //     "created_at" => Carbon::now(),
-            //     "updated_at" => Carbon::now()
-            // ],
-            // [
-            //     "name" => "Batch-2",
-            //     "course_id" => "1",
-            //     "instructor_id" => "1",
-            //     "start_date" => Carbon::now()->toDateString(),
-            //     "end_date" => Carbon::now()->addDay(30)->toDateString(),
-            //     "start_time" => Carbon::now()->toTimeString(),
-            //     "end_time" => Carbon::now()->addHours(3)->toTimeString(),
-            //     "status" => "1",
-            //     "created_at" => Carbon::now(),
-            //     "updated_at" => Carbon::now()
-            // ],
-            // [
-            //     "name" => "Batch-2",
-            //     "course_id" => "2",
-            //     "instructor_id" => "1",
-            //     "start_date" => Carbon::now()->toDateString(),
-            //     "end_date" => Carbon::now()->addDay(30)->toDateString(),
-            //     "start_time" => Carbon::now()->toTimeString(),
-            //     "end_time" => Carbon::now()->addHours(3)->toTimeString(),
-            //     "status" => "1",
-            //     "created_at" => Carbon::now(),
-            //     "updated_at" => Carbon::now()
-            // ],
-            // [
-            //     "name" => "Batch-3",
-            //     "course_id" => "1",
-            //     "instructor_id" => "1",
-            //     "start_date" => Carbon::now()->toDateString(),
-            //     "end_date" => Carbon::now()->addDay(30)->toDateString(),
-            //     "start_time" => Carbon::now()->toTimeString(),
-            //     "end_time" => Carbon::now()->addHours(3)->toTimeString(),
-            //     "status" => "1",
-            //     "created_at" => Carbon::now(),
-            //     "updated_at" => Carbon::now()
-            // ],
-            // [
-            //     "name" => "Batch-3",
-            //     "course_id" => "2",
-            //     "instructor_id" => "1",
-            //     "start_date" => Carbon::now()->toDateString(),
-            //     "end_date" => Carbon::now()->addDay(30)->toDateString(),
-            //     "start_time" => Carbon::now()->toTimeString(),
-            //     "end_time" => Carbon::now()->addHours(3)->toTimeString(),
-            //     "status" => "1",
-            //     "created_at" => Carbon::now(),
-            //     "updated_at" => Carbon::now()
-            // ],
-        // ]);
     }
 }
