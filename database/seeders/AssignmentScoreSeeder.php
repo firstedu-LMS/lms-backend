cb3ab85e383f45b74b39c7c1b1d0177cb36f67c0
Axel Philip <axel8102005@gmail.com>
2023-11-13 16:09:03 +0630

    assignmentScore

---
+++ database/seeders/AssignmentScoreSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AssignmentScoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('assignment_scores')->insert([
+            [
+                "course_id" => 1,
+                "batch_id" => 1,
+                "assignment_id" => 3,
+                "student_id" => 1,
+                "score" => 80,
+                "created_at" => Carbon::now(),
+                "updated_at" => Carbon::now()
+            ]
+            ]);
+    }
+}
