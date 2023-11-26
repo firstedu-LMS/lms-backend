aeede57e560c95df477928c5289f6379fb9382f0
virnus <myatkyawt974@gmail.com>
2023-11-26 14:17:31 +0630

    merge coursecontroller with instructor

--- routes/instructor.php
+++ routes/instructor.php
@@ -4,11 +4,10 @@ use App\Http\Controllers\Admin\WeekController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Instructor\InstructorController;
-use App\Http\Controllers\Instructor\CourseController;
 
 
 Route::middleware('auth:sanctum')->group(function () {
-        Route::get('courses',[CourseController::class,'index']);
+        Route::get('courses',[InstructorController::class,'getInstructorCourses']);
         Route::get('profile', [InstructorController::class, 'profile']);
         Route::patch('profile-update', [InstructorController::class, 'update']);
         Route::patch('profile-name-update', [InstructorController::class, 'updateName']);
