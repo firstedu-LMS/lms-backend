3d7349af0830d97ad192e694afa63be4703c6345
virnus <myatkyawt974@gmail.com>
2023-11-26 14:51:50 +0630

    refactor and add lesson for instructor

--- routes/instructor.php
+++ routes/instructor.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\LessonController;
 use App\Http\Controllers\Admin\WeekController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,13 +8,22 @@ use App\Http\Controllers\Instructor\InstructorController;
 
 
 Route::middleware('auth:sanctum')->group(function () {
-        Route::get('courses',[InstructorController::class,'getInstructorCourses']);
-        Route::get('profile', [InstructorController::class, 'profile']);
-        Route::patch('profile-update', [InstructorController::class, 'update']);
-        Route::patch('profile-name-update', [InstructorController::class, 'updateName']);
-        Route::post('profile-image-update', [InstructorController::class, 'updateImage']);
-        Route::post('change-password', [InstructorController::class, 'changePassword']);
-        Route::get('courses/{batch_id}/weeks', [WeekController::class , 'index']);
-        Route::post('courses/{batch_id}/weeks' , [WeekController::class , 'store']);
+        Route::controller(InstructorController::class)->group(function () {
+            Route::get('courses','getInstructorCourses');
+            Route::get('profile',  'profile');
+            Route::patch('profile-update',  'update');
+            Route::patch('profile-name-update',  'updateName');
+            Route::post('profile-image-update',  'updateImage');
+            Route::post('change-password',  'changePassword');
+        });
+        Route::controller(LessonController::class)->group(function(){
+            Route::post('lessons','store');
+            Route::get('lessons/{week_id}','index');
+        });
+        Route::controller(WeekController::class)->group(function(){
+            Route::get('courses/{batch_id}/weeks', 'index');
+            Route::post('courses/{batch_id}/weeks' , 'store');
+        });
+      
     });
 ?>
